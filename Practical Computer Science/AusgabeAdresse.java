39869173d4855130c1a69e4b8dc118ece37f0f64
JuDev <jahidudd65@gmail.com>
2022-09-08 16:45:58 +0200

    Pushing exercises

---
+++ Practical Computer Science/AusgabeAdresse.java
@@ -0,0 +1,9 @@
+public class AusgabeAdresse {    
+  
+       public static void main(String[] args) {
+              System.out.println("Vorname:\tFriedrich");
+              System.out.println("Nachname:\tList\n");
+              System.out.println("Strasse:\tBrunhildenstr. 142");
+              System.out.println("Ort:\t\t65189 Wiesbaden");
+       }
+}
---
+++ Practical Computer Science/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {    
+  
+       public static void main(String[] args) {
+              System.out.println("Hello World!");
+    
+              System.out.println("00");
+              System.out.println(00);
+              System.out.println(1 + 2);
+              System.out.println(+1-2);
+    
+              System.out.println("1" + 2);
+              System.out.println("1" + 2 + 3);
+              System.out.println(1 + " und " + 2);
+       }
+}

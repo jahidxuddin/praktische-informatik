e959134bf95dbfbb8cdf05eb22b0f90622e705b7
JuDev <jahidudd65@gmail.com>
2022-09-22 15:12:36 +0200

    New stuff

---
+++ Practical Computer Science/Benzinverbrauch.java
@@ -0,0 +1,61 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/*
+    Berechnung des Benzinvebrauches
+*/
+
+public class Benzinverbrauch {
+
+    int letzterKilometerstand;
+    int aktuellerKilometerstand;
+    int getankeLiter;
+
+    float zurückgelegteKilometer;
+    String benzinverbauch;
+
+    public void titel() {
+        System.out.println("****************************************************************");
+        System.out.println("\t\tBerechnung Benzinverbrauch");
+        System.out.println("****************************************************************");
+    }
+
+    public void eingabe() { // Abfrage der benötigten Daten
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nEingabe:");
+        
+        System.out.print("Bitte geben Sie den Kilometerstand beim letzten Tanken ein: ");
+        letzterKilometerstand = scanner.nextInt();
+
+        System.out.print("Bitte geben Sie den aktuellen Kilometerstand ein: ");
+        aktuellerKilometerstand = scanner.nextInt();
+
+        System.out.print("Bitte geben Sie die Menge der getankten Liter Benzin ein: ");
+        getankeLiter = scanner.nextInt();
+
+        scanner.close();
+    }
+
+    public void berechnen() { // Berechnung des Benzinverbrauchs
+        zurückgelegteKilometer = aktuellerKilometerstand - letzterKilometerstand;
+
+        DecimalFormat formatter = new DecimalFormat("#.##");
+        benzinverbauch = formatter.format(getankeLiter * 100.00f / zurückgelegteKilometer);
+    }
+    
+    public void ausgabe() { // Ausgabe des Benzinverbauchs
+        System.out.println("\nAuswertung:");
+        System.out.println("Der durchschnittliche Benzinverbrauch beträgt " + benzinverbauch + " Liter");
+        System.out.println("pro gefahrene 100 Kilometer, wenn Sie " + zurückgelegteKilometer +" Kilometer gefahren");    
+        System.out.println("sind und " + (float) getankeLiter + " Liter getankt haben.");
+    }
+
+    public static void main(String[] args) {
+        Benzinverbrauch rechner = new Benzinverbrauch();
+        rechner.titel();
+        rechner.eingabe();
+        rechner.berechnen();
+        rechner.ausgabe();
+    }
+}
---
+++ Practical Computer Science/Zinsen.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+/*
+    Berechnung der Zinsen
+*/
+
+public class Zinsen {
+  
+    double kaptial;
+    double zinssatz;
+    int laufzeitInJahre;
+  
+    String zusatz;
+    double zinsen;
+    double endKapital;
+  
+    DecimalFormat oneDecimal = new DecimalFormat("#.#");
+    DecimalFormat twoDecimal = new DecimalFormat("#.##");
+
+    public void titel() {
+        System.out.println("********************************************");
+        System.out.println("\t     Berechnung Zinsen");
+        System.out.println("********************************************\n");
+    }
+
+    public void eingabe() { // Abfrage benötigten Daten
+        Scanner scanner = new Scanner(System.in);
+    
+        System.out.print("Bitte geben Sie das Startkapital ein: ");
+        kaptial = scanner.nextDouble();
+        
+        System.out.print("Bitte geben Sie den Zinssatz ein: ");
+        zinssatz = scanner.nextDouble();
+    
+        System.out.print("Bitte geben Sie die Laufzeit in Jahre ein: ");
+        laufzeitInJahre = scanner.nextInt();
+
+        scanner.close();
+    }
+
+    public void berechnen() { // Berechnung der Zinsen
+        if (laufzeitInJahre == 1) {
+          zusatz = "Jahr";
+        } else {
+          zusatz = "Jahre";
+        }
+        zinsen = (kaptial * zinssatz * laufzeitInJahre) / 100;
+        endKapital = kaptial * (1 + laufzeitInJahre * (zinssatz / 100)); 
+    }
+
+    public void ausgabe() { // Ausgabe der Ergebnisse 
+        System.out.println("\nBei einem Startkapital von " + kaptial + " EUR und Zins-");
+        System.out.println("satz von " + oneDecimal.format(zinssatz) + "% liegt das Kaptial nach ");
+        System.out.println(laufzeitInJahre + " " + zusatz + " und Zinsen von ");
+        System.out.println(twoDecimal.format(zinsen) + " EUR bei " + twoDecimal.format(endKapital) + " EUR.");
+    }
+
+    public static void main(String[] args) {
+        Zinsen zinsen = new Zinsen();
+        zinsen.titel();
+        zinsen.eingabe();
+        zinsen.berechnen();
+        zinsen.ausgabe();
+    }
+}

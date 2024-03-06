586d1360e4bc10c2ec7798585daf7a8fb29cf3e9
ju-dev-17 <jahidudd65@gmail.com>
2024-03-06 08:18:04 +0100

    Migrate to none oop syntax

--- Ergänzender Grundkurs/tastatur-eingabe-input-stream/src/main/java/de/ju/TastaturEingabeInputStream.java
+++ Ergänzender Grundkurs/tastatur-eingabe-input-stream/src/main/java/de/ju/TastaturEingabeInputStream.java
@@ -1,44 +1,36 @@
-package de.ju;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class TastaturEingabeInputStream {
-    public TastaturEingabeInputStream() {
-        ausgabe();
+char leseZeichen() {
+    System.out.print("Bitte geben Sie ein Zeichen an: ");
+
+    try {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        return (char) reader.read();
+    } catch (IOException e) {
+        throw new RuntimeException(e);
     }
+}
 
-    public char leseZeichen() {
-        System.out.print("Bitte geben Sie ein Zeichen an: ");
+String leseWort() {
+    System.out.print("Bitte geben Sie mehrere Zeichen an: ");
 
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    try {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-            return (char) reader.read();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        return reader.readLine();
+    } catch (IOException e) {
+        throw new RuntimeException(e);
     }
+}
 
-    public String leseWort() {
-        System.out.print("Bitte geben Sie mehrere Zeichen an: ");
+void ausgabe() {
+    System.out.println(leseZeichen());
+    System.out.println(leseWort());
+}
 
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-            return reader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void ausgabe() {
-        System.out.println(leseZeichen());
-        System.out.println(leseWort());
-    }
-
-    public static void main(String[] args) {
-        new TastaturEingabeInputStream();
-    }
+void main() {
+   ausgabe();
 }

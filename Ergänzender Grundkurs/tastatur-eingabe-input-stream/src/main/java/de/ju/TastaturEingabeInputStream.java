f458d13fe36271fcd6e27d54555aa787260ee35d
ju-dev-17 <jahidudd65@gmail.com>
2024-03-06 09:05:04 +0100

    Minimized code

--- Ergänzender Grundkurs/tastatur-eingabe-input-stream/src/main/java/de/ju/TastaturEingabeInputStream.java
+++ Ergänzender Grundkurs/tastatur-eingabe-input-stream/src/main/java/de/ju/TastaturEingabeInputStream.java
@@ -5,9 +5,7 @@ import java.io.InputStreamReader;
 char leseZeichen() {
     System.out.print("Bitte geben Sie ein Zeichen an: ");
 
-    try {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
         return (char) reader.read();
     } catch (IOException e) {
         throw new RuntimeException(e);
@@ -19,11 +17,8 @@ void leseWort() {
     while (!input.equalsIgnoreCase("over")) {
         System.out.print("Bitte geben Sie mehrere Zeichen an: ");
 
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             input = reader.readLine();
-
             System.out.println(input);
         } catch (IOException e) {
             throw new RuntimeException(e);

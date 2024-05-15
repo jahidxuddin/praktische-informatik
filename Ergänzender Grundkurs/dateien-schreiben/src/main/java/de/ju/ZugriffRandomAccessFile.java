aa31140dab964d6b7a042baf870683c69b0b077a
Jahid Uddin <jahidudd65@gmail.com>
2024-05-15 09:01:32 +0200

    RandomAccessFile Writing

--- Ergänzender Grundkurs/dateien-schreiben/src/main/java/de/ju/ZugriffRandomAccessFile.java
+++ Ergänzender Grundkurs/dateien-schreiben/src/main/java/de/ju/ZugriffRandomAccessFile.java
@@ -1,10 +1,34 @@
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.math.BigInteger;
+
+void datenSpeichern() {
+    try (RandomAccessFile raf = new RandomAccessFile("fakultätenBigInt.txt", "rw")) {
+        for (int i = 1; i < 22; i++) {
+            BigInteger fakultaet = BigInteger.ONE;
+            for (int j = 2; j < i + 1; j++) {
+                fakultaet = fakultaet.multiply(BigInteger.valueOf(j));
+            }
+            raf.writeBytes("\t\tFakultaet von " + i + "! = " + fakultaet + "\n");
+        }
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
+}
+
+void datenLesen() {
+    try (RandomAccessFile raf = new RandomAccessFile("fakultätenBigInt.txt", "rw")) {
+        raf.seek(0);
+        String text;
+        while ((text = raf.readLine()) != null) {
+            System.out.println(text);
+        }
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
+}
 
 void main() {
-    try (RandomAccessFile raf = new RandomAccessFile("test.txt", "w")) {
-        raf.writeUTF("AAAAA\nBBBBB\nCCCCC");
-    } catch (IOException e) {
-        System.out.println("Datei konnte nicht gelesen/beschrieben werden.");
-    }
+    datenSpeichern();
+    datenLesen();
 }
